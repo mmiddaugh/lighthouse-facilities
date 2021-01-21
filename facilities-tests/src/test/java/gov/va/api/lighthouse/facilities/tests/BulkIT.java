39c2fd16006e3944d9a3ec8f7172d62edbe87c83
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2021-01-21 09:54:23 -0500

    Added logging for bulkit for testing.

--- facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/BulkIT.java
+++ facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/BulkIT.java
@@ -2,6 +2,7 @@ package gov.va.api.lighthouse.facilities.tests;
 
 import static gov.va.api.lighthouse.facilities.tests.FacilitiesRequest.facilitiesRequest;
 
+import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.lighthouse.facilities.api.v0.GeoFacilitiesResponse;
 import org.junit.jupiter.api.Test;
 
@@ -10,28 +11,50 @@ public class BulkIT {
 
   @Test
   void all_csv() {
+    ExpectedResponse response =
     facilitiesRequest("text/csv", ALL_PATH, 200);
+
+    System.out.println("ALL CSV: \n" + response);
   }
 
   @Test
   void all_geoJson() {
-    facilitiesRequest("application/geo+json", ALL_PATH, 200)
-        .expectValid(GeoFacilitiesResponse.class);
+    ExpectedResponse response =  facilitiesRequest("application/geo+json", ALL_PATH, 200);
+
+    System.out.println("ALL GEOJSON: \n" + response);
+
+    response.expectValid(GeoFacilitiesResponse.class);
   }
 
   @Test
   void all_json() {
-    facilitiesRequest("application/json", ALL_PATH, 200).expectValid(GeoFacilitiesResponse.class);
+
+    ExpectedResponse response =
+    facilitiesRequest("application/json", ALL_PATH, 200);
+
+    System.out.println("ALL JSON: \n" + response);
+
+            response.expectValid(GeoFacilitiesResponse.class);
+
   }
 
   @Test
   void all_noAccept() {
-    facilitiesRequest(null, ALL_PATH, 200).expectValid(GeoFacilitiesResponse.class);
+    ExpectedResponse response =
+    facilitiesRequest(null, ALL_PATH, 200);
+
+    System.out.println("ALL NOACCEPT: \n" + response);
+
+            response.expectValid(GeoFacilitiesResponse.class);
   }
 
   @Test
   void all_vndGeoJson() {
-    facilitiesRequest("application/vnd.geo+json", ALL_PATH, 200)
-        .expectValid(GeoFacilitiesResponse.class);
+    ExpectedResponse response =
+    facilitiesRequest("application/vnd.geo+json", ALL_PATH, 200);
+
+    System.out.println("ALL VNDGEOJSON: \n" + response);
+
+        response.expectValid(GeoFacilitiesResponse.class);
   }
 }
